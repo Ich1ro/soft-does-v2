fa2b9f71a19474788293ad3c76f0978f40c14473
PavloHohish <pasha.steam@ukr.net>
2025-02-24 08:31:38 +0200

    added services page

--- src/sections/services/hero/hero.tsx
+++ src/sections/services/hero/hero.tsx
@@ -1,37 +1,19 @@
-import data from '@texts/main/index.json';
+import serviceHero from "@assets/images/main/service-hero_shadow.png";
 
-import kansasFlag from '@assets/images/main/kansas-flag-min.jpg';
-import serviceHero from '@assets/images/main/service-hero.png';
-
-import s from './hero.module.scss';
-import Image from 'next/image';
+import s from "./hero.module.scss";
+import Image from "next/image";
 
 export const Hero = () => {
   return (
     <section className={s.root}>
       <div className={s.container}>
-        <div className={s.tagContainer}>
-          <p className={s.tagText}>{data.services_hero.tagText}</p>
-          <Image
-            aria-hidden
-            alt=""
-            src={kansasFlag.src}
-            className={s.flagImage}
-            width={kansasFlag.width}
-            height={kansasFlag.height}
-          />
-        </div>
-        <h1 className={s.title}>{data.services_hero.title}</h1>
-        <button className={s.button}>{data.services_hero.buttonText}</button>
-        <Image
-            aria-hidden
-            alt=""
-            src={serviceHero.src}
-            className={s.heroImage}
-            width={serviceHero.width}
-            height={serviceHero.height}
-          />
+        <h1 className={s.title}>
+          <span>Soft does</span> services
+        </h1>
+        <p className={s.tagText}>We transform market insights into globally beloved apps that power your business</p>
+        <button className={s.button}>Contact us</button>
       </div>
+      <Image aria-hidden alt="" src={serviceHero.src} className={s.heroImage} width={serviceHero.width} height={serviceHero.height} />
     </section>
   );
 };
