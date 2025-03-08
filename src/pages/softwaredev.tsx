4db3f66a7ff506152bb78ba4a2ca3728da324784
PavloHohish <pasha.steam@ukr.net>
2025-03-08 02:38:30 +0200

    softwaredev page

--- src/pages/softwaredev.tsx
+++ src/pages/softwaredev.tsx
@@ -1,7 +1,12 @@
 import { SEO } from "@shared/ui/seo";
 import { ROUTES } from "@shared/constants/routes";
 
-import { Hero } from "@sections/expertise/hero/hero";
+import { Hero } from "@sections/softwareDev/hero/hero";
+import { Results } from "@sections/softwareDev/results/results";
+import { Technologies } from "@sections/technologies/technologies";
+import { Services } from "@sections/softwareDev/services/services";
+import { CaseStudies } from "@sections/case-studies/case-studies";
+import { Engagement } from "@sections/engagement-options/engagement-options";
 import { Solutions } from "@sections/solutions/solutions";
 import { Industries } from "@sections/industries/industries";
 
@@ -13,6 +18,11 @@ export default function SoftwareDev() {
       <SEO title="SOFT DOES" description="SOFT DOES is company, that will help your bussines grow!" pathname={ROUTES.softwareDevelopment} />
       <Layout>
         <Hero />
+        <Results />
+        <Technologies />
+        <Services />
+        <CaseStudies />
+        <Engagement />
         <Solutions />
         <Industries />
       </Layout>
